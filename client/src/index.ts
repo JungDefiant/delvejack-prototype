122df163f76cf0a227e9e65e973c177a48e45190
Cristian Habib <42555252+JungDefiant@users.noreply.github.com>
2023-12-22 18:12:36 -0800

    Remade folders

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,9 +1,31 @@
-import Phaser from 'phaser';
-import config from './config';
-import GameScene from './scenes/Game';
+import Phaser from "phaser";
 
-new Phaser.Game(
-  Object.assign(config, {
-    scene: [GameScene]
-  })
-);
+// custom scene class
+export class GameScene extends Phaser.Scene {
+    preload() {
+      // preload scene
+    }
+
+    create() {
+      // create scene
+    }
+
+    update(time: number, delta: number): void {
+      // game loop
+    }
+}
+
+// game config
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#b6d53c',
+    parent: 'phaser-example',
+    physics: { default: "arcade" },
+    pixelArt: true,
+    scene: [ GameScene ],
+};
+
+// instantiate the game
+const game = new Phaser.Game(config);
