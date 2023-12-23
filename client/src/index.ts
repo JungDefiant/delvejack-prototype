e4f1243c8b316960c2faf6172da184b1c5d80f63
Cristian Habib <42555252+JungDefiant@users.noreply.github.com>
2023-12-22 18:31:02 -0800

    Removed tracking for node modules, added .gitignore

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,17 +1,86 @@
+import { Client, Room } from "colyseus.js";
 import Phaser from "phaser";
 
 // custom scene class
 export class GameScene extends Phaser.Scene {
+
+    // Phase Room Settings
+    client = new Client("ws://localhost:2567");
+    room: Room;
+    playerEntities: {[sessionId: string]: any} = {};
+
+    // Input Settings
+    inputPayload = {
+        left: false,
+        right: false,
+        up: false,
+        down: false,
+    };
+    cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+    
     preload() {
       // preload scene
+      this.load.image('ship_0001', 'https://cdn.glitch.global/3e033dcd-d5be-4db4-99e8-086ae90969ec/ship_0001.png');
+
+      this.cursorKeys = this.input.keyboard.createCursorKeys();
     }
 
-    create() {
-      // create scene
+    async create() {
+      console.log("Joining room...");
+
+      try {
+        this.room = await this.client.joinOrCreate("my_room");
+        console.log("Joined successfully!");
+
+      } catch (e) {
+        console.error(e);
+      }
+
+      if(!this.room) {
+        return;
+      }
+
+      this.room.state.players.onAdd((player, sessionId) => {
+        //
+        // A player has joined!
+        //
+        console.log("A player has joined! Their unique session id is", sessionId);
+
+        const entity = this.physics.add.image(player.x, player.y, 'ship_0001');
+
+        // keep a reference of it on `playerEntities`
+        this.playerEntities[sessionId] = entity;
+
+        // listening for server updates
+        player.onChange(() => {
+            // update local position immediately
+            entity.x = player.x;
+            entity.y = player.y;
+        });
+      });
+
+      this.room.state.players.onRemove((player, sessionId) => {
+        const entity = this.playerEntities[sessionId];
+        if (entity) {
+            // destroy entity
+            entity.destroy();
+    
+            // clear local reference
+            delete this.playerEntities[sessionId];
+        }
+    });
     }
 
     update(time: number, delta: number): void {
-      // game loop
+      // skip loop if not connected with room yet.
+      if (!this.room) { return; }
+
+      // send input to the server
+      this.inputPayload.left = this.cursorKeys.left.isDown;
+      this.inputPayload.right = this.cursorKeys.right.isDown;
+      this.inputPayload.up = this.cursorKeys.up.isDown;
+      this.inputPayload.down = this.cursorKeys.down.isDown;
+      this.room.send(0, this.inputPayload);
     }
 }
 
---
+++ server/src/rooms/GameRoom.ts
@@ -0,0 +1,60 @@
+import { Room, Client } from "@colyseus/core";
+import { RoomState } from "./schema/RoomState";
+import { Player } from "./schema/Player";
+
+export class GameRoom extends Room<RoomState> {
+  maxClients = 4;
+
+  onCreate(options: any) {
+    this.setState(new RoomState());
+
+    // handle player input
+    this.onMessage(0, (client, input) => {
+      // get reference to the player who sent the message
+      const player = this.state.players.get(client.sessionId);
+      const velocity = 2;
+
+      if (input.left) {
+        player.x -= velocity;
+
+      } else if (input.right) {
+        player.x += velocity;
+      }
+
+      if (input.up) {
+        player.y -= velocity;
+
+      } else if (input.down) {
+        player.y += velocity;
+      }
+    });
+  }
+
+  onJoin(client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
+
+    const mapWidth = 800;
+    const mapHeight = 600;
+
+    // create Player instance
+    const player = new Player();
+
+    // place Player at a random position
+    player.x = (Math.random() * mapWidth);
+    player.y = (Math.random() * mapHeight);
+
+    // place player in the map of players by its sessionId
+    // (client.sessionId is unique per connection!)
+    this.state.players.set(client.sessionId, player);
+  }
+
+  onLeave(client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
+    this.state.players.delete(client.sessionId);
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+
+}
